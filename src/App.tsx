9a93ec5973a73df52e16341f7f79c0cb5fc3193d
SIVAKRISHNA <krishnasiva6745@gmail.com>
2025-12-27 23:01:28 +0530

    feat: Day 01 useDebounce hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css';
+import Day01 from './days/Day01/Day01';
 
 function App() {
     return (
         <>
-            <h1>App</h1>
+            <h1 className="text-4xl font-bold mb-8">Architecture Labs</h1>
+            <Day01 />
         </>
     );
 }
---
+++ src/days/Day01/Day01.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import useDebounce from './useDebounce';
+
+const Day01 = () => {
+    const [inputValue, setInputValue] = useState<string>('');
+    const debouncedValue = useDebounce(inputValue, 500);
+
+    return (
+        <div className="flex flex-col gap-4 justify-center items-center">
+            <input
+                type="text"
+                name="text"
+                placeholder="type something..."
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+                className="border p-2 rounded"
+            />
+            <span>{debouncedValue}</span>
+        </div>
+    );
+};
+
+export default Day01;
