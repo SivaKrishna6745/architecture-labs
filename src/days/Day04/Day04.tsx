1ff82e1f7e20aada0b3fc4b96e9235c6bdf5b84f
SIVAKRISHNA <krishnasiva6745@gmail.com>
2026-01-03 15:15:08 +0530

    feat: Add heading to Day04

--- src/days/Day04/Day04.tsx
+++ src/days/Day04/Day04.tsx
@@ -3,6 +3,7 @@ import FileExplorer from './FileExplorer';
 const Day04 = () => {
     return (
         <div className="flex flex-col items-center">
+            <h2 className="text-3xl mb-4">File Explorer</h2>
             <FileExplorer />
         </div>
     );
